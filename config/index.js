cd557193bbc4af4243c5a6b913fff6832203122d
pengforfight <pzuopin@163.com>
2019-07-21 19:06:16 +0800

    更改路径

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-pay-demo/dist/',
 
     /**
      * Source Maps
